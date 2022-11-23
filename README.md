473fac7b77ccd8e95caad0cf69ede9524c053512
Crlziito04 <105681853+Crlziito04@users.noreply.github.com>
2022-11-22 21:04:39 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-fcieol
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-fcieol)
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+
+import TweetListContainer from './components/TweetListContainer/TweetListContainer';
+
+export default function App() {
+  return (
+    <div>
+      <Semaforo />
+    </div>
+  );
+}
